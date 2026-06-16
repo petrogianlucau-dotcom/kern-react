7d55097fcbfa4b4ef0a4d12a3731cb5289a37001
petrogianlucau-dotcom <petro.gianlucau@gmail.com>
2026-06-16 15:36:14 -0300

    Restaurar cursor y scroll reveal en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Cursor from './components/Cursor'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import WhatsApp from './components/WhatsApp'
@@ -7,11 +8,15 @@ import Servicios from './pages/Servicios'
 import Casos from './pages/Casos'
 import FAQ from './pages/FAQ'
 import Contacto from './pages/Contacto'
+import useScrollReveal from './components/useScrollReveal'
 import './App.css'
 
 function App() {
+  useScrollReveal()
+
   return (
     <BrowserRouter>
+      <Cursor />
       <Navbar />
       <main>
         <Routes>
